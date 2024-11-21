7ae08f0ef5f69a08dbc04f384541f0646878288c
Øyvind Skaaden <oyvindskaaden@gmail.com>
2024-11-21 17:20:54 +0100

    Cleaned the tasks and some testing

--- src/info.lua
+++ src/info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "Base Plugin",
   Version = "0.0",
-  BuildVersion = "0.0.0.10",
+  BuildVersion = "0.0.0.11",
   Id = "096ddefc-1d3f-4d66-bd01-49b1fdcc5fde",
   Author = "Your_Company",
   Description = "A very basic plugin"  
